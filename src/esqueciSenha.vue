c21f4d47921282f51e892246cc95dced4f9efcca
Victor Hugo de Almeida Magalhaes <magalhaes.victor@hcosta.com.br>
2023-10-04 17:29:42 -0300

    commit 4

--- src/esqueciSenha.vue
+++ src/esqueciSenha.vue
@@ -68,7 +68,9 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-image: url(assets/877280.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
   z-index: 1;
 }
 
